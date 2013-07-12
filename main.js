483007ebeb9498ef6b6647deded9e325e8cc174e
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-07-12 18:16:46 -0400

    Got regex and script finding implemented

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-var outputFile;
+/*  
+
+This file is part of nibble.
+
+nibble is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+nibble is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with nibble.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 // Require the stuff we need
 
@@ -18,14 +35,28 @@ server.on('request',function(req,res){
 	// Determine file to serve
 	
 	var fileToServe = req.url.toString();
-	fileToServe = fileToServe.substr(1,fileToServe.length);
-	
-	console.log("Serving " + fileToServe);
-	
-	// Create file and serve it
 
-	readFile(fileToServe,res);
+	// Serve index.nib if nothing else is present
 	
+	if(fileToServe == "/"){
+	
+		console.log("Serving index.nib");
+	
+		// Create file and serve it
+
+		generateAndServe('index.nib',res);
+		
+	
+	}else{
+
+		fileToServe = fileToServe.substr(1,fileToServe.length);
+	
+		console.log("Serving " + fileToServe);
+	
+		// Create file and serve it
+
+		generateAndServe(fileToServe,res);
+	}
 	
 });
 
@@ -35,12 +66,22 @@ server.listen(port);
 
 console.log("Listening on port " + port);
 
-function readFile(filepath,res){
+function generateAndServe(filepath,res){
+
+	// Read in the raw file based on file path
 
 	fs.readFile(filepath,function(err,data){
 		if(data){
+			
+			// Contains the raw data in the file
+			var outputFile;
+
+			// Turn in into a string
 			outputFile = data.toString();
-			beginParsing(res);
+			
+			// Begin the generation and serving of the .nib file
+
+			beginParsing(res, outputFile);
 		}	
 	});
 
@@ -48,14 +89,41 @@ function readFile(filepath,res){
 
 // Actual parsing going on here
 
-function beginParsing(res){
+function beginParsing(res, outputFile){
 
-	// Log file
+	// Log file to console for debugging purposes
+	
 	console.log(outputFile);
 
-	// Modify file
+	/** PARSING AND GENERATION OF MARKUP GOES HERE
+	//	
+	//	Heavily uses regular expressions
+	//
+	*/
 
-	// Serve it
+	// Variable that stores the chunks of scripts found in the .nib file
+
+	var allScripts = new Array();
+
+    // Regex for finding code that is script
+
+    var scriptTagsRegex = /<\?nib[\s\S]+\?>/im;
+
+    // Log all the scripts to the console
+
+	if((allScripts = scriptTagsRegex.exec(outputFile)) !== null){
+	    
+	    for(var i = 0; i < allScripts.length; i++){
+	    	console.log("Found scripts:\n" + allScripts[i])
+	    }
+	    
+	}
+
+
+
+
+	// Serve the fully generated outputFile
+
 	res.writeHeader(200, {"Content-Type": "text/html"});
 	res.write(outputFile);
 	res.end();
